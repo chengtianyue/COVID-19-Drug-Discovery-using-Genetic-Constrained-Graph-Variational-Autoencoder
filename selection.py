c6aa47eb5659dab6cdd0b04c102a5344ba04cd4d
vweovuvwevwe <68412465+vweovuvwevwe@users.noreply.github.com>
2020-08-10 17:47:16 +0800

    Add files via upload

--- selection.py
+++ selection.py
@@ -1,18 +1,38 @@
 import random
-def select(flist, crat=0.25):
+
+def getflist(molist,choosingfunc):
+	#输出包含每个分子适应值的列表
+	flist = []
+	for a in molist:
+		flist.append(choosingfunc(a))
+	return flist
+
+def select(molist,flist,reproduction_metric=2):
 	#轮盘赌选择+精英保留
 	#flist为含有每个样本适应度值的list，crat为希望选择进下一轮的样本比例
+	comp = []
+	for a in molist:
+		comp.append([a])
+	for a in range(len(flist)):
+		comp[a].append(flist[a])
 	chosen = []
-	flist.sort()
-	lenf = len(flist)-1
-	cnumb = int(lenf * crat/2)
-	#进入下一轮的样本数量的1/2
+	n = len(flist)
+	for i in range(n-1):
+		for j in range(i+1,n):
+			if comp[i][1]<comp[j][1]: 
+				comp[i],comp[j]=comp[j],comp[i]	
+	lenf = len(flist) - 1
+	cnumb = int(lenf / (2*(reproduction_metric**2))) + 1
+	#进入下一轮的样本数量为1/(默认4)
 	for a in range(cnumb):
-		chosen.append(flist[lenf-a])
-		del flist[-1]
+		chosen.append(comp[0][0])
+		del comp[0]
 	#选择适应值最高的一部分样本进入下一轮
 	for a in range(cnumb):
-		chosen.append(random.choice(flist))
+		chosen.append(random.choice(comp)[0])
 	#再随机选择一部分样本进入下一轮
-	print(chosen)
+	molist = chosen
 	return chosen
+
+def score(molecule):
+	return -len(molecule)
